ec650ef659fe4266a31fb80b2c1677fb4379a05a
Ramald <ramaldesmts@gmail.com>
2026-08-22 07:50:06 -0300

    feat: implementa entidade conta

---
+++ src/main/java/com/ramaldes/newpay/model/Conta.java
@@ -0,0 +1,45 @@
+package com.ramaldes.newpay.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String numeroConta;
+    private BigDecimal saldo;
+
+    public Conta() {}
+
+    public Conta(String numeroConta, BigDecimal saldo) {
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+}
